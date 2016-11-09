44d1329d74328bf8d5daba35d8fe15e600280025
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-09 11:50:13 +0300

    Add method executing automatically on client and server

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 
 class LinkCreate extends Component {
+  handleSubmit(event){
+    event.preventDefault();
+    console.log(this.refs.input.value);
+
+    let url = this.refs.input.value;
+
+    Meteor.call('links.insert', url)
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to shorten</label>
-          <input className="form-control" />
+          <input className="form-control" ref="input"/>
         </div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
